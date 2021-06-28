5a79ec99892f3081a5ee40902750a44cbd1452c2
Invazing <invazing@hotmail.com>
2021-06-28 09:52:12 -0300

    Adicionar arquivos de projeto.

---
+++ Cobranca/Forms/Cobranca.Designer.vb
@@ -0,0 +1,127 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Cobranca
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Cobranca))
+        Me.cod_cliente = New System.Windows.Forms.TextBox()
+        Me.DTGcliente = New System.Windows.Forms.DataGridView()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.nometxb = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        CType(Me.DTGcliente, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'cod_cliente
+        '
+        Me.cod_cliente.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper
+        Me.cod_cliente.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cod_cliente.Location = New System.Drawing.Point(85, 14)
+        Me.cod_cliente.Name = "cod_cliente"
+        Me.cod_cliente.Size = New System.Drawing.Size(82, 23)
+        Me.cod_cliente.TabIndex = 3
+        '
+        'DTGcliente
+        '
+        Me.DTGcliente.AllowUserToAddRows = False
+        Me.DTGcliente.AllowUserToDeleteRows = False
+        Me.DTGcliente.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+        Me.DTGcliente.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCellsExceptHeaders
+        Me.DTGcliente.BackgroundColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.DTGcliente.Cursor = System.Windows.Forms.Cursors.Default
+        Me.DTGcliente.GridColor = System.Drawing.Color.Blue
+        Me.DTGcliente.Location = New System.Drawing.Point(4, 48)
+        Me.DTGcliente.Name = "DTGcliente"
+        Me.DTGcliente.ReadOnly = True
+        Me.DTGcliente.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.DTGcliente.Size = New System.Drawing.Size(776, 313)
+        Me.DTGcliente.TabIndex = 4
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(6, 16)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(73, 17)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "CODIGO :"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(173, 16)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(55, 17)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "NOME :"
+        '
+        'nometxb
+        '
+        Me.nometxb.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper
+        Me.nometxb.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.nometxb.Location = New System.Drawing.Point(234, 14)
+        Me.nometxb.Name = "nometxb"
+        Me.nometxb.Size = New System.Drawing.Size(522, 23)
+        Me.nometxb.TabIndex = 6
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.cod_cliente)
+        Me.GroupBox1.Controls.Add(Me.nometxb)
+        Me.GroupBox1.Location = New System.Drawing.Point(4, 1)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(776, 43)
+        Me.GroupBox1.TabIndex = 8
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Consulta"
+        '
+        'Cobranca
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(788, 369)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.DTGcliente)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Cobranca"
+        Me.Text = "COBRANÇA"
+        CType(Me.DTGcliente, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents cod_cliente As TextBox
+    Friend WithEvents DTGcliente As DataGridView
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents nometxb As TextBox
+    Friend WithEvents GroupBox1 As GroupBox
+End Class
---
+++ Cobranca/Forms/Cobranca.vb
@@ -0,0 +1,29 @@
+﻿Imports FirebirdSql.Data.FirebirdClient
+Public Class Cobranca
+    Dim conexaoFB As FbConnection
+    Dim da As FbDataAdapter
+    Dim ds As New DataSet
+    Private Sub Cobranca_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        conexaoFB = New FbConnection("Server=localhost;User=SYSDBA;Password=masterkey;Database=C:\SIGE.FDB")
+        ' DABASE NÃO INCLUSO (BAIXAR NO MEGA)
+    End Sub
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles cod_cliente.TextChanged
+        pesquisar()
+    End Sub
+    Public Sub pesquisar()
+        conexaoFB.Open()
+        da = New FbDataAdapter("Select ID_PESSOA as CODIGO,RAZAO as NOME,FANTASIA as RAZAO from GE_PESSOA where ID_PESSOA LIKE '%" + cod_cliente.Text + "%' AND RAZAO LIKE '%" + nometxb.Text + "%'", conexaoFB)
+        Try
+            da.Fill(ds, "Cadastro")
+            DTGcliente.DataSource = ds.Tables(0)
+            Dim dt As DataTable = New DataTable
+            da.Fill(dt)
+            DTGcliente.DataSource = dt
+        Catch ex As Exception
+        End Try
+        conexaoFB.Close()
+    End Sub
+    Private Sub Nometxb_TextChanged(sender As Object, e As EventArgs) Handles nometxb.TextChanged
+        pesquisar()
+    End Sub
+End Class
